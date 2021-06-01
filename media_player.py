84f3ecaa41ad3d280234e8a2746fda70c43b2b65
DuHa <david.havrlant@ibillboard.com>
2021-06-01 20:25:45 +0200

    websocket functional version

--- media_player.py
+++ media_player.py
@@ -1,10 +1,11 @@
 """Platform for sensor integration."""
-import websockets
+import websocket
 import json
 import asyncio
 import requests
 from homeassistant.const import TEMP_CELSIUS
 from homeassistant.helpers.entity import Entity
+from homeassistant.helpers import entity_platform
 from homeassistant.components.media_player import MediaPlayerEntity
 from homeassistant.const import (
     STATE_IDLE,
@@ -32,64 +33,88 @@ ATTR_TO_PROPERTY = [
     ATTR_MEDIA_TITLE,
     ATTR_MEDIA_DURATION,
     ATTR_MEDIA_POSITION,
-    'video_ids',
-    'instance_ids'
+    'video_id',
+    'instance_id'
 ]
 
 
 SUPPORT_FEATURES = (
     SUPPORT_PAUSE
     | SUPPORT_PLAY
-    | SUPPORT_SELECT_SOURCE
     | SUPPORT_PLAY_MEDIA
     | SUPPORT_NEXT_TRACK
     | SUPPORT_PREVIOUS_TRACK
 )
 
-is_alive = True
 
-async def alive():
-    while is_alive:
-        _LOGGER.debug('alive')
-        await asyncio.sleep(300)
+import threading
+import websocket
 
 
-async def async_processing(callback):
-    async with websockets.connect('ws://localhost:9989') as websocket:
-        while True:
-            try:
-                message = await websocket.recv()
-                _LOGGER.debug(message)
-                callback(json.loads(message))
+class Controller:
+    def __init__(self, hass, config, platform):
+        self.hass = hass
+        self.config = config
+        self.platform = platform
+        self.youtube_instances = []
 
-            except websockets.exceptions.ConnectionClosed:
-                _LOGGER.error('ConnectionClosed')
-                is_alive = False
-                break
+    async def handle_state_change(self, change):
+        _LOGGER.debug(change["type"])
+        if change["type"] == "YoutubeInstanceAdded":
+            _LOGGER.debug("DAVID: adding new entity")
+            instance = YoutubeMediaPlayer(self.hass, self.config, change["data"]["id"])
+            self.youtube_instances.append(instance)
+            await self.platform.async_add_entities([instance])
+            
+        elif change["type"] == "YoutubeInstanceRemoved":
+            _LOGGER.debug("DAVID: removing entity")
+            for index, instance in enumerate(self.youtube_instances):
+                if instance.instance_id == change["data"]["id"]:
+                    _LOGGER.debug(instance.entity_id)
+                    await self.platform.async_remove_entity(instance.entity_id)
+                    self.youtube_instances.pop(index)
+                    break
+            
+        elif change["type"] == "YoutubeInstanceChanged":
+            _LOGGER.debug("DAVID: updating entity")
+            for index, instance in enumerate(self.youtube_instances):
+                if instance.instance_id == change["data"]["id"]:
+                    _LOGGER.debug(instance.entity_id)
+                    instance.handle_state_change(change)
+                    break
 
-
-def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     _LOGGER.debug("setting youtube")
-    instance = YoutubeMediaPlayer(hass, config, 5)
-    async_add_entities([instance])
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    tasks = [
-        asyncio.ensure_future(alive()),
-        asyncio.ensure_future(async_processing(instance.handle_state_change))
-    ]
-    loop.run_until_complete(asyncio.wait(tasks))
+    platform = entity_platform.current_platform.get()
+    controller = Controller(hass, config, platform)
+
+    def on_message(ws, message):
+        asyncio.run(controller.handle_state_change(json.loads(message)))
+
+    def on_error(ws, error):
+        _LOGGER.debug(error)
+
+    def on_close(ws):
+        _LOGGER.debug("### closed ###")
+
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp("ws://localhost:9989",
+                              on_message = on_message,
+                              on_error = on_error,
+                              on_close = on_close)
+    wst = threading.Thread(target=ws.run_forever)
+    wst.daemon = True
+    wst.start()
 
 
 class YoutubeMediaPlayer(MediaPlayerEntity):
     """Representation of a Sensor."""
 
-    def __init__(self, hass, config, instanceId):
+    def __init__(self, hass, config, instance_id):
         """Initialize the sensor."""
-        self._state = []
+        self._state = {}
         self.hass = hass
-        self.currentYoutubeInstanceId = "-1"
-        self.instanceId = instanceId
+        self._instance_id = instance_id
         self.youtubeControllerUrl = config['youtube_controller_url']
 
     @property
@@ -98,65 +123,44 @@ class YoutubeMediaPlayer(MediaPlayerEntity):
 
     @property
     def state(self):
-        def callback(item):
-            if not "video" in item or not "isPlaying" in item["video"]:
-                return STATE_IDLE
-            elif item["video"]["isPlaying"]:
-                return STATE_PLAYING
-            else:
-                return STATE_PAUSED
-        return json.dumps(self.do_for_all(callback))
+        if not "video" in self._state or not "isPlaying" in self._state["video"]:
+            return STATE_IDLE
+        elif self._state["video"]["isPlaying"]:
+            return STATE_PLAYING
+        else:
+            return STATE_PAUSED
     
     @property
-    def instance_ids(self):
-        def callback(item):
-            if "instanceId" in item:
-                return item["instanceId"]
-            else:
-                return None
-        return self.do_for_all(callback)
+    def instance_id(self):
+        return self._instance_id
     
     @property
     def media_title(self):
-        def callback(item):
-            if "video" in item and "title" in item["video"]:
-                return item["video"]["title"]
-            else:
-                return None
-        return self.do_for_all(callback)
+        if "video" in self._state and "title" in self._state["video"]:
+            return self._state["video"]["title"]
+        else:
+            return None
 
     @property
     def media_duration(self):
-        def callback(item):
-            if "video" in item and "duration" in item["video"]:
-                return item["video"]["duration"]
-            else:
-                return None
-        return self.do_for_all(callback)
+        if "video" in self._state and "duration" in self._state["video"]:
+            return self._state["video"]["duration"]
+        else:
+            return None
 
     @property
     def media_position(self):
-        def callback(item):
-            if "video" in item and "currentPosition" in item["video"]:
-                return item["video"]["currentPosition"]
-            else:
-                return None
-        return self.do_for_all(callback)
+        if "video" in self._state and "currentPosition" in self._state["video"]:
+            return self._state["video"]["currentPosition"]
+        else:
+            return None
 
     @property
-    def video_ids(self):
-        def callback(item):
-            if "video" in item and "videoId" in item["video"]:
-                return item["video"]["videoId"]
-            else:
-                return None
-        return self.do_for_all(callback)
-
-    def do_for_all(self, fn):
-        array = []
-        for index, item in enumerate(self._state):
-            array.append(fn(item))
-        return array
+    def video_id(self):
+        if "video" in self._state and "videoId" in self._state["video"]:
+            return self._state["video"]["videoId"]
+        else:
+            return None
 
     @property
     def supported_features(self):
@@ -175,41 +179,26 @@ class YoutubeMediaPlayer(MediaPlayerEntity):
     def should_poll(self):
         return False
 
-    async def async_select_source(self, source):
-        """Set the input source."""
-        self.currentYoutubeInstanceId = source
-
     def media_play(self):
         """Send play command."""
-        requests.post(self.youtubeControllerUrl + '/play?youtubeInstanceId=' + self.currentYoutubeInstanceId)
+        requests.post(self.youtubeControllerUrl + '/play?youtubeInstanceId=' + str(self.instance_id))
 
     def media_pause(self):
         """Send pause command."""
-        requests.post(self.youtubeControllerUrl + '/pause?youtubeInstanceId=' + self.currentYoutubeInstanceId)
+        requests.post(self.youtubeControllerUrl + '/pause?youtubeInstanceId=' + str(self.instance_id))
 
     def play_media(self, media_type, media_id, **kwargs):
         """Play media from a URL or file."""
-        requests.post(self.youtubeControllerUrl + '/watch?youtubeInstanceId=' + self.currentYoutubeInstanceId, json={'videoId':media_id})
+        requests.post(self.youtubeControllerUrl + '/watch?youtubeInstanceId=' + str(self.instance_id, json={'videoId':media_id}))
 
     async def async_media_previous_track(self):
         """Fire the media previous action."""
-        requests.post(self.youtubeControllerUrl + '/watch-previous?youtubeInstanceId=' + self.currentYoutubeInstanceId)
+        requests.post(self.youtubeControllerUrl + '/watch-previous?youtubeInstanceId=' + str(self.instance_id))
     
     async def async_media_next_track(self):
         """Fire the media next action."""
-        requests.post(self.youtubeControllerUrl + '/watch-next?youtubeInstanceId=' + self.currentYoutubeInstanceId)
+        requests.post(self.youtubeControllerUrl + '/watch-next?youtubeInstanceId=' + str(self.instance_id))
 
     def handle_state_change(self, change):
-        if change["type"] == "YoutubeInstanceAdded":
-            self._state.append({"instanceId": change["data"]["id"]})
-        if change["type"] == "YoutubeInstanceRemoved":
-            for index, item in enumerate(self._state):
-                if item["instanceId"] == change["data"]["id"]:
-                    self._state.pop(index)
-                    break
-        if change["type"] == "YoutubeInstanceChanged":
-            for index, item in enumerate(self._state):
-                if item["instanceId"] == change["data"]["id"]:
-                    self._state[index]["video"] = change["data"]["videoInfo"]
-                    break
+        self._state["video"] = change["data"]["videoInfo"]
         self.schedule_update_ha_state()
